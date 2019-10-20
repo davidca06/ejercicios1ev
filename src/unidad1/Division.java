495dcf6f15ad004ff411b876a3873d9536d28a4d
David Castillo Alvarez <deivizcastle@gmail.com>
2019-10-20 13:50:30 +0200

    "Ejercicio 1 resuelto"

---
+++ src/unidad1/Division.java
@@ -0,0 +1,16 @@
+package unidad1;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		float division;
+		a=1234;
+		b=532;
+		division=a/b;
+		System.out.printf("%d : %d = %15.2f",a,b,division);
+
+	}
+
+}
